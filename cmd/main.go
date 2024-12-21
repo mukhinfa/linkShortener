1349d1f3db7020336f94df4587b1b75423991137
muhinfa <muhinfa@gmail.com>
2024-12-21 15:14:42 +0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/muhinfa/linkShortener/configs"
+	"github.com/muhinfa/linkShortener/internal/auth"
+
+	"net/http"
+)
+
+func main() {
+	conf := configs.LoadConfig()
+	router := http.NewServeMux()
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
+}
